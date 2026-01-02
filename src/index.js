cd3506f264a7f3f2b9a7a3a052e2df21cfbf42d0
Rohit B <rohetbiswash@gmail.com>
2026-01-02 21:46:39 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<React.StrictMode>
+    <div>
+        <h1>Hello Vrother!</h1>
+    </div>
+</React.StrictMode>
+);
