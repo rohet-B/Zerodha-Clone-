08204accdce4b14f43a5be8defe1dd2c240aed0e
Rohit B <rohetbiswash@gmail.com>
2026-01-05 00:22:53 +0530

    5th Commit

--- src/Landing Page/Navbar.js
+++ src/Landing Page/Navbar.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <h1>Navbar</h1>
-  )
-}
+    <nav className="navbar navbar-expand-lg border-bottom" style={{ backgroundColor: "#FFF" }}>
+      <div className="container p-2">
+        <a className="navbar-brand" href="#">
+          <img src="media/Images/logo.svg" alt="Logo" style={{ width: "25%" }} />
+        </a>
 
-export default Navbar
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Signup</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Product</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Pricing</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Support</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
