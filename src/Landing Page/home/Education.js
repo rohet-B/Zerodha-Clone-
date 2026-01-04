f7488a5ad32dc16efa88ceba4db0e1612cdffc98
Rohit B <rohetbiswash@gmail.com>
2026-01-04 23:03:04 +0530

    4th Commit

--- src/Landing Page/home/Education.js
+++ src/Landing Page/home/Education.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Education = () => {
   return (
-    <h1>Education</h1>
+    <div className='container mt-5'>
+      <div className='row'>
+        <div className='col-6'>
+          <img src='media/Images/education.svg'></img>
+        </div>
+        <div className='col-6'>
+          <h1 className='mb-3 fs-2'>Free and open market education</h1>
+          <p>Varsity, the largetst online stock market education book in the world covering everything fromt he basics to advance trading.</p>
+          <a href='' style={{textDecoration:'none'}}>Versity<i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+          <p className='mt-5'>TradingQ&A, the most active trading and investment community in India for all your market realted queries.</p>
+          <a href='' style={{textDecoration:'none'}}>Trading Q&A<i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+        </div>
+      </div>
+    </div>
   )
 }
 
